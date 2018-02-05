6f9febba543e56eda5a7c7fb2c65250ec649a65e
alaahayba <alaahayba14@gmail.com>
2018-02-05 20:10:45 +0200

    register and login

--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,8 @@ from . import views
 urlpatterns = [
         path('', views.UserFormView.as_view(), name='register'),
         path('home/', views.userLogout.as_view(), name='logout'),
+        # path('home/', views.userLogout.as_view(), name='logout'),
         #path('logout/', logout, {'next_page': settings.LOGOUT_REDIRECT_URL}, name='logout')
-        path('users/', views.hell, name='user'),
-        path('hello/', views.hi, name='hh'),
+        # path('users/', views.hell, name='user'),
+        # path('hello/', views.hi, name='hh'),
 ]
--- users/views.py
+++ users/views.py
@@ -8,13 +8,14 @@ from django.template import loader
 from django.http import Http404
 from django.contrib.auth.models import User
 from django.views.decorators.http import require_http_methods
-
+from django.http import HttpResponse
 
 # def CheckLogin():
 #     if request.user.is_authenticated():
 #         return redirect('bar')
 #     else:
 #         return redirect('bar')
+
 
 class userLogout(View):
     #@login_required
@@ -34,19 +35,16 @@ class UserFormView(View):
 
     register_form=UserForm
     login_form=UserFormLogin
+    registerForm=register_form(None)
+    loginForm=login_form(None)
     template_name='users/registration_form.html'
 
     #need to display form
     def get(self,request):
         if not request.user.is_authenticated:
-            registerForm=self.register_form(None)
-            loginForm=self.login_form(None)
-
-            return render(request,self.template_name,{'registerform':registerForm,'loginForm':loginForm})
+            return render(request,self.template_name,{'registerform':self.registerForm,'loginForm':self.loginForm})
         else:
-            #return render(request,'users/hello.html')
             return redirect('logout')
-
 
     # need to submit and save in DB
     def username_present(self,username):
@@ -54,28 +52,44 @@ class UserFormView(View):
             return True
         return False
 
+   ###########login action
+    def login_view(self,request):
+        uname =request.POST['username']
+        pword =request.POST['password']
+        user = authenticate(username=uname, password=pword)
+        if user is not None:
+            return user
+        else:
+            return False
+###################################################
     def post(self,request):
         if request.POST.get('submit') == 'register':
             form=self.register_form(request.POST)
-            loginForm=self.login_form(None)
             if form.is_valid():
                 user=form.save(commit=False)
                 username=form.cleaned_data['username']
                 if not self.username_present(username):
                     username=form.cleaned_data['email']
                     password=form.cleaned_data['password']
-                    #user.set_password(password)
+                    user.set_password(password)
                     user.save()
                     login(request,user)
-                    return render(request,'users/hello.html')
+                    return redirect('logout')
 
                 else:
                     raise forms.ValidationError(u'Username "%s" is already in use.' % username)
-                    #return render(request,self.template_name,{'registerform':form})
-            return render(request,self.template_name,{'registerform':form,'loginForm':loginForm})
-        else:
-            template_name='users/bas.html'
-            return render(request,template_name)
+                    return render(request,self.template_name,{'registerform':form,'loginForm':self.loginForm})
+
+        elif request.POST.get('submit') == 'login':
+            user = self.login_view(request)
+            if user:
+                login(request,user)
+                return redirect('logout')
+            # else:
+            #     raise self.loginForm.ValidationError('Eroor')
+        return render(request,self.template_name,{'registerform':self.registerForm,'loginForm':self.loginForm})
+
+
 
 def hell(request):
     template_name='users/base.html'
