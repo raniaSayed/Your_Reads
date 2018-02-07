5f224f4c73fbf653fe5de4207505e48223fdd508
alaahayba <alaahayba14@gmail.com>
2018-02-07 09:12:56 +0200

    add read and rate and wishlist tables

--- books/views.py
+++ books/views.py
@@ -3,8 +3,6 @@ from django.http import HttpResponse
 from django.http import JsonResponse
 from django.http import HttpResponseRedirect
 from django.shortcuts import render,redirect
-
-
 from books.models import Book
 
 # Create your views here.
