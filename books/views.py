40ac0c3447563d19ac4599f54c998bafa7872b6b
raniaSayed <raniasayed313@gmail.com>
2018-02-09 17:47:53 +0200

    enhance single book page

--- books/views.py
+++ books/views.py
@@ -13,19 +13,25 @@ from math import ceil
 def index(request):
     books = Book.objects.all()
     categories = Category.objects.all()
-    # return HttpResponse(book)
-    # return JsonResponse({'name': 'Ahmed'})
-    # return HttpResponseRedirect("bo")
     return render(request,'index.html',{'books':books,'categories':categories,'checked' : range(3),'unchecked':range(2) })
-    # return redirect('/admin',{'name':'ahmed'})
 
-################ except for get book category ###################
+
 def view(request,id):
     #make a middleware for this function to prevent unexistant ids
+    book = Book.objects.get(id=id)
+    summary = book.summary
     rate = rateList.objects.filter(book_id = id ).aggregate(Avg('rate'))["rate__avg"]
-    # book_categories = Book.objects.filter(id = id).categories
-    return render(request,"single.html",{"book" :Book.objects.get(id=id),'category':"",'checked' : rate,'unchecked':ceil(5-int(rate))});
-    # return JsonResponse({"rate":book_categories})
+    if(rate is None):
+        rate = 0
+
+    categories = book.categories.all()
+    unchecked = ceil(5-int(rate))
+
+    return render(request,"single.html",
+    {"book":book,"summary":summary,
+    'categories':categories,
+    'checked' : rate,'unchecked':unchecked});
+    # return HttpResponse(categories[1])
 
 # return books of specific author
 def get_author_books(request,id):
